8ba3df5be30d884e53d0d20d35a1623333f737ef
thushitharan <thushitharansutha@gmail.com>
2023-10-25 21:03:08 +0530

    UI changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,3 +260,17 @@
   new PureCounter();
 
 })()
+
+
+
+//Scroll Progress Bar
+function progressBarScroll() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+      height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+      scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+window.onscroll = function () {
+  progressBarScroll();
+};
